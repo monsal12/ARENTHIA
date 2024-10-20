70889e1024ffc323115454685e8f46e59ac19dfa
macbook <macbook@macbooks-MacBook-Air.local>
2024-10-20 10:36:25 +0700

    fitur

--- Data/gachaArmorPool.js
+++ Data/gachaArmorPool.js
@@ -22,28 +22,6 @@ const armorPool = [
         imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297278103873126460/IMG_8794.jpg?ex=671557e2&is=67140662&hm=ef99d9a301e8d3a80ae670958698de9657aff20c2abf00dba84f881a66ef39e3&=&format=webp&width=455&height=569", 
         rate: 0.25 
     },
-    { 
-        name: "Phoenix Feather Robe", 
-        grade: "Epic", 
-        statBoost: { 
-            strength: 4, 
-            intelligence: 8, 
-            ability: 6
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297278104158474381/IMG_8795.jpg?ex=671557e2&is=67140662&hm=04458c88bb8029ee086a77d12b61896ac18fce72ced8ba4561e4a609fbcff8a6&=&format=webp&width=325&height=568", 
-        rate: 0.1 
-    },
-    { 
-        name: "Celestial Guardian Armor", 
-        grade: "Legendary", 
-        statBoost: { 
-            strength: 10, 
-            intelligence: 5, 
-            ability: 8
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297278104430968944/IMG_8796.jpg?ex=671557e2&is=67140662&hm=931bdb508adade93547cbff63dda1afcb63084e77666b9fe9e45257f55cfc93a&=&format=webp&width=573&height=569", 
-        rate: 0.04 
-    }
     // Tambahkan armor lain sesuai kebutuhan
 ];
 
