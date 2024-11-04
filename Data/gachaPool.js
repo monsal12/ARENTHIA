3fc0c37c1202c387b183104fd17ee3ff59f15332
monsal12 <salmanda.230510102@mhs.unimal.ac.id>
2024-11-04 14:40:06 +0700

    sk

--- Data/gachaPool.js
+++ Data/gachaPool.js
@@ -1,59 +1,205 @@
 const gachaPool = [
-    { 
-        name: "Dragon Fang Saber", 
-        grade: "Common", 
-        statBoost: { 
-            strength: 5, 
-            intelligence: 0, 
-            ability: 2 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1295648234466709545/374e6f2d-095f-4b51-ad5d-937676bdeff2.webp?ex=670f69f3&is=670e1873&hm=729f68ec3710dff8cd943cc03bdbe36bc4879a863b222377b472a359e60fa8d3&=&format=webp&width=350&height=350", // Ganti dengan URL gambar yang sesuai
-        rate: 0.5 
-    },
-    { 
-        name: "Phoenix Feather Spear", 
-        grade: "Common", 
-        statBoost: { 
-            strength: 6, 
-            intelligence: 0, 
-            ability: 3 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1295679248409034784/c4c0f1cc-d69e-4dba-bcfc-45330c821ba2.webp?ex=670f86d5&is=670e3555&hm=34d586d2c1e62eab02dab5a95dbad282fb35864925a09ae1328c8f2f25832599&=&format=webp&width=585&height=585", 
-        rate: 0.5 
-    },
-    { 
-        name: "Celestial Fan", 
-        grade: "Rare", 
-        statBoost: { 
-            strength: 0, 
-            intelligence: 8, 
-            ability: 2 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1295679785749577838/IMG_8691.jpg?ex=670f8755&is=670e35d5&hm=30a6fc9f05b19e1dd4b68362a4e312dd9f3bdd7479001e4d65a3de8714e974fa&=&format=webp&width=583&height=585", 
-        rate: 0.25 
-    },
-    { 
-        name: "White Tiger Claws", 
-        grade: "Rare", 
-        statBoost: { 
-            strength: 8, 
-            intelligence: 0, 
-            ability: 2 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1295678525596110928/0e71e567-9dfd-4201-8e16-c43882d43953-0.png?ex=670f8629&is=670e34a9&hm=6b02756cbef255c1eadfb201c8e7e6e1ee29866ea0a69e6ff1d9a3fdd6f53580&=&format=webp&quality=lossless&width=585&height=585", 
-        rate: 0.25 
-    },
-    { 
-        name: "Vermilion Bird Bow", 
-        grade: "Epic", 
-        statBoost: { 
-            strength: 8, 
-            intelligence: 1, 
-            ability: 3 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1295678525977661450/IMG_8685.jpg?ex=670f8629&is=670e34a9&hm=09f863d8f761d9817fc477a825e336f46790a766e6e67be152a14b7bbe321341&=&format=webp", 
-        rate: 0.1 
-    },
+
+            { 
+                "name": "Skeletal Scythe", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 10, 
+                    "intelligence": 5, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889205520928769/IMG_9064.jpg?ex=6729c1a1&is=67287021&hm=0a0139b581d0ca04e9defc8ee558220f5c1344db19c09a0fc21fe8e1061604f5&=&format=webp", 
+                "rate": 0.1 
+            },
+            { 
+                "name": "Witch's Broomstick", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 4, 
+                    "intelligence": 8, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889217084620830/IMG_9065.jpg?ex=6729c1a3&is=67287023&hm=a8a47c58ceec2b1855369fb818edc3e5393b82d7adbbb01544b0e9f85a60f1a8&=&format=webp&width=366&height=350", 
+                "rate": 0.4 
+            },
+            { 
+                "name": "Pumpkin Launcher", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 12, 
+                    "intelligence": 3, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889232175464468/IMG_9066.jpg?ex=6729c1a7&is=67287027&hm=c3e8a5d6bac9c1283404af88e94316e0c9b8e7abbbc2bc6fcc28d40a00ee6e7d&=&format=webp&width=280&height=498", 
+                "rate": 0.2
+            },
+            { 
+                "name": "Ghostly Rapier", 
+                "grade": "rare", 
+                "statBoost": { 
+                    "strength": 2, 
+                    "intelligence": 4, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889284763648010/IMG_9067.jpg?ex=6729c1b4&is=67287034&hm=3e621f3110c000ca21af317cffbf24fa12c95b77448e3a74b177735ab7df8560&=&format=webp&width=498&height=498", 
+                "rate": 0.5 
+            },
+            { 
+                "name": "Vampire's Fang", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 5, 
+                    "intelligence": 6, 
+                    "ability": 1 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889285070098483/IMG_9068.jpg?ex=6729c1b4&is=67287034&hm=1bb54ae58d0cd7244d81940b93314704dba0090ef4add441e08314b56d67a98b&=&format=webp&width=376&height=498", 
+                "rate": 0.4 
+            },
+            { 
+                "name": "Cursed Bow", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 8, 
+                    "intelligence": 5, 
+                    "ability": 2 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889285350850570/IMG_9069.jpg?ex=6729c1b4&is=67287034&hm=86240f0a914aece275dfd6d519d2cb964e7a79ae54953978bd6b31b5a522e211&=&format=webp&width=498&height=498", 
+                "rate": 0.1 
+            },
+            { 
+                "name": "Zombie Hand", 
+                "grade": "rare", 
+                "statBoost": { 
+                    "strength": 2, 
+                    "intelligence": 2, 
+                    "ability": 2 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889285720084521/IMG_9070.jpg?ex=6729c1b4&is=67287034&hm=d74ae29909f79c66ed0dc33310dbc5a764226639b354112950d2b04766356457&=&format=webp&width=498&height=498", 
+                "rate": 0.7 
+            },
+            { 
+                "name": "Haunted Staff", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 4, 
+                    "intelligence": 8, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889286084853772/IMG_9071.jpg?ex=6729c1b4&is=67287034&hm=1498143c70ad1788c18cc018416c40d363f3b6888b97b4dccafb712ba134b005&=&format=webp&width=498&height=498", 
+                "rate": 0.3 
+            },
+            { 
+                "name": "Mummy's Curse", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 10, 
+                    "intelligence": 5, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889286324191243/IMG_9072.jpg?ex=6729c1b4&is=67287034&hm=779b7212b175d5b47f0603555f8ce12b52fc85cf558a317d4408a089896ab84a&=&format=webp", 
+                "rate": 0.1 
+            },
+            { 
+                "name": "Bat Wings", 
+                "grade": "rare", 
+                "statBoost": { 
+                    "strength": 0, 
+                    "intelligence": 3, 
+                    "ability": 3 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889286584242249/IMG_9073.jpg?ex=6729c1b4&is=67287034&hm=9bb61bc50b655e77f3fe8fab6620b209073561cf8d8c23f3bebe0dab93454b5a&=&format=webp&width=498&height=498", 
+                "rate": 0.6 
+            },
+            { 
+                "name": "Wicked Axe", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 15, 
+                    "intelligence": 0, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889286839828480/IMG_9074.jpg?ex=6729c1b4&is=67287034&hm=00956f532aa229d7f5cbdf478d6681340e36b5821543442d0493228f54ead5ab&=&format=webp&width=334&height=498", 
+                "rate": 0.2
+            },
+            { 
+                "name": "Spider Whip", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 3, 
+                    "intelligence": 7, 
+                    "ability": 2 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889287062130751/IMG_9075.jpg?ex=6729c1b4&is=67287034&hm=aba4192605f8149adcbdfe8fcf8fb35ceba54bc157fdf862a3fa08ca6366cc86&=&format=webp", 
+                "rate": 0.4 
+            },
+            { 
+                "name": "Creepy Claws", 
+                "grade": "rare", 
+                "statBoost": { 
+                    "strength": 3, 
+                    "intelligence": 3, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889287330562078/IMG_9076.jpg?ex=6729c1b4&is=67287034&hm=036ef7b47fe7a91981d159ea69b8c241f19b163e9e5add1d646cdb7663c3deec&=&format=webp", 
+                "rate": 0.7 
+            },
+            { 
+                "name": "Spectral Blade", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 13, 
+                    "intelligence": 2, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889301733806090/IMG_9077.jpg?ex=6729c1b8&is=67287038&hm=86e7da657415df1d6cc21fdca17bb7c0b95cbc3e6bb08f63c4e3bb3a410cd203&=&format=webp&width=498&height=498", 
+                "rate": 0.1 
+            },
+            { 
+                "name": "Cauldron Staff", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 4, 
+                    "intelligence": 8, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889301973012521/IMG_9078.jpg?ex=6729c1b8&is=67287038&hm=ef31d41eb4ded9854449e424dd37c9c78a93d925e5ddcbaf3d61aed5e4da2263&=&format=webp&width=374&height=498", 
+                "rate": 0.3 
+            },
+            { 
+                "name": "Grave Digger's Shovel", 
+                "grade": "epic", 
+                "statBoost": { 
+                    "strength": 12, 
+                    "intelligence": 3, 
+                    "ability": 0 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889302258090065/IMG_9079.jpg?ex=6729c1b8&is=67287038&hm=c73a1fb7d8bae7a2cb973ff766be914e3a5f783dd04fe469fb04b3178bcb54ee&=&format=webp&width=336&height=498", 
+                "rate": 0.1
+            },
+            { 
+                "name": "Wraith's Sickle", 
+                "grade": "rare", 
+                "statBoost": { 
+                    "strength": 2, 
+                    "intelligence": 2, 
+                    "ability": 2 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889302576992308/IMG_9080.jpg?ex=6729c1b8&is=67287038&hm=6e0a7f0cbd2b403a1ef51fe00007e71e8d06cd362c6306c967765fcae75c4a11&=&format=webp&width=332&height=498", 
+                "rate": 0.6 
+            },
+            { 
+                "name": "Black Cat's Claw", 
+                "grade": "ultra rare", 
+                "statBoost": { 
+                    "strength": 6, 
+                    "intelligence": 4, 
+                    "ability": 2 
+                }, 
+                "imageUrl": "https://media.discordapp.net/attachments/1302888987987283998/1302889303025778710/IMG_9081.jpg?ex=6729c1b8&is=67287038&hm=7ffb31cdaa2d2394668d7dd54efa28e931d8fa594a93d0bcc5ea425971ac34eb&=&format=webp", 
+                "rate": 0.4 
+            }
+
+    
 ];
 const totalRate = gachaPool.reduce((sum, weapon) => sum + weapon.rate, 0);
 gachaPool.forEach(weapon => {
--- Data/gachapoolAksesoris.js
+++ Data/gachapoolAksesoris.js
@@ -1,37 +1,117 @@
 const gachaPoolAksesoris = [
-    { 
-        name: "Moonlight Pendant", 
-        grade: "Common", 
-        statBoost: { 
-            strength: 2, 
-            intelligence: 3, 
-            ability: 1 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297552882647826483/IMG_8804.jpg?ex=671657ca&is=6715064a&hm=004efb8202676b80ac1976d64443daa186e255c99ccebfe46ae2252f47eb4416&=&format=webp&width=569&height=569", // Ganti dengan URL gambar yang sesuai
-        rate: 0.6 
-    },
-    { 
-        name: "Sapphire Ring", 
-        grade: "Rare", 
-        statBoost: { 
-            strength: 0, 
-            intelligence: 6, 
-            ability: 4 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297552882907877376/IMG_8805.jpg?ex=671657ca&is=6715064a&hm=d0e3eaf4ad790a9e88b4ebce8e456142405d4eaa194c6f5d7b416299a107d938&=&format=webp&width=569&height=569", 
-        rate: 0.3 
-    },
-    { 
-        name: "Golden Crown", 
-        grade: "Epic", 
-        statBoost: { 
-            strength: 5, 
-            intelligence: 8, 
-            ability: 6 
-        }, 
-        imageUrl: "https://media.discordapp.net/attachments/1256628479768657962/1297552883130044436/IMG_8806.jpg?ex=671657ca&is=6715064a&hm=163db20002b6e16d4cea61aad9db1bd043a3170c230fe01e958715db183d862b&=&format=webp&width=427&height=569", 
-        rate: 0.1 
-    }
+    [
+        { 
+            "name": "Cursed Ring", 
+            "grade": "epic", 
+            "statBoost": { 
+                "strength": 0, 
+                "intelligence": 8, 
+                "ability": 7 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897218797178881/IMG_9098.jpg?ex=6729c917&is=67287797&hm=853fb238716753b7f6637eff39220a4919806c0af92b080fb8432681409c0bb3&=&format=webp&width=498&height=498", 
+            "rate": 0.1
+        },
+        { 
+            "name": "Witch's Hat", 
+            "grade": "ultra rare", 
+            "statBoost": { 
+                "strength": 2, 
+                "intelligence": 10, 
+                "ability": 0 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897219086843946/IMG_9099.jpg?ex=6729c917&is=67287797&hm=10a4fdf6e5a7bd5e568079e12537825cc6de801579773f385d892ed305d927d5&=&format=webp&width=498&height=498", 
+            "rate": 0.3 
+        },
+        { 
+            "name": "Pumpkin Amulet", 
+            "grade": "epic", 
+            "statBoost": { 
+                "strength": 5, 
+                "intelligence": 6, 
+                "ability": 4 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897219388702740/IMG_9100.jpg?ex=6729c917&is=67287797&hm=ad0589090601021705802b254dd8aeda0eaae484d0913878403cce4bf4db449c&=&format=webp&width=498&height=498", 
+            "rate": 0.2 
+        },
+        { 
+            "name": "Ghostly Pendant", 
+            "grade": "rare", 
+            "statBoost": { 
+                "strength": 1, 
+                "intelligence": 3, 
+                "ability": 2 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897219732766771/IMG_9101.jpg?ex=6729c917&is=67287797&hm=6b82b68fa13f7b175130427e619e6cc399b9db960d98759207c314d9911f87ed&=&format=webp&width=498&height=498", 
+            "rate": 0.6 
+        },
+        { 
+            "name": "Vampire Fangs Necklace", 
+            "grade": "ultra rare", 
+            "statBoost": { 
+                "strength": 3, 
+                "intelligence": 4, 
+                "ability": 5 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897220059791430/IMG_9102.jpg?ex=6729c917&is=67287797&hm=103d334c0997ce416b754e3743c473b257a4a913c93410a2600594a863d9155c&=&format=webp&width=384&height=497", 
+            "rate": 0.4 
+        },
+        { 
+            "name": "Banshee's Earrings", 
+            "grade": "epic", 
+            "statBoost": { 
+                "strength": 0, 
+                "intelligence": 12, 
+                "ability": 3 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897220433215508/IMG_9103.jpg?ex=6729c918&is=67287798&hm=d3d1deba3925836dc3f5b81499468aa6ad1f9493f8c7a2becb8f126b0b40b9ac&=&format=webp&width=498&height=498", 
+            "rate": 0.2 
+        },
+        { 
+            "name": "Zombie Finger Ring", 
+            "grade": "rare", 
+            "statBoost": { 
+                "strength": 2, 
+                "intelligence": 2, 
+                "ability": 2 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897220710043679/IMG_9104.jpg?ex=6729c918&is=67287798&hm=58b4225f03e2cdcf1733f4501361699de824817e71a456057cb77458870e581f&=&format=webp&width=498&height=498", 
+            "rate": 0.7 
+        },
+        { 
+            "name": "Haunted Brooch", 
+            "grade": "ultra rare", 
+            "statBoost": { 
+                "strength": 1, 
+                "intelligence": 8, 
+                "ability": 3 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897220982407199/IMG_9105.jpg?ex=6729c918&is=67287798&hm=ca126cea4a61f1ef93321f87d59939d84a29356876385f3efeed67403ebbf571&=&format=webp&width=664&height=498", 
+            "rate": 0.3 
+        },
+        { 
+            "name": "Eerie Charm Bracelet", 
+            "grade": "epic", 
+            "statBoost": { 
+                "strength": 4, 
+                "intelligence": 7, 
+                "ability": 4 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897221259497483/IMG_9106.jpg?ex=6729c918&is=67287798&hm=20fcbe5f74688216463afd1dc133adcbbb3a63e47c9790cfa4b880128392ff1e&=&format=webp&width=332&height=498", 
+            "rate": 0.1 
+        },
+        { 
+            "name": "Black Cat's Tail Charm", 
+            "grade": "rare", 
+            "statBoost": { 
+                "strength": 1, 
+                "intelligence": 3, 
+                "ability": 2 
+            }, 
+            "imageUrl": "https://media.discordapp.net/attachments/1302897152204345384/1302897221561483355/IMG_9107.jpg?ex=6729c918&is=67287798&hm=568edd5d53e5117b0c3a11489c99d10f5c73cc8cd855dd28b688df70008e209f&=&format=webp&width=427&height=498", 
+            "rate": 0.6 
+        }
+    ]
+    
 ];
 
 // Helper function to pull a random accessory based on rates
